32a0f8024b7b42e4a1de5f4ffa1dc835344b99b3
Arpit Agarwal <arpitjindal97@gmail.com>
2022-08-05 02:05:48 +0530

    Add indexer to workflow

--- build/builder.sh
+++ build/builder.sh
@@ -5,6 +5,5 @@ docker build -t temp:latest . -f "${PWD}"/build/Dockerfile
 
 docker build -t arpitjindal1997/apiserver:latest . -f "${PWD}"/build/apiserver/Dockerfile
 docker build -t arpitjindal1997/refresher:latest . -f "${PWD}"/build/refresher/Dockerfile
-
 docker push arpitjindal1997/apiserver:latest
 docker push arpitjindal1997/refresher:latest
