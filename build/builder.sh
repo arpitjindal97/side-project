4a024ce18ce06490ca5f53522b5d7a943fa14b7d
Arpit Agarwal <arpitjindal97@gmail.com>
2022-08-05 01:08:39 +0530

    Add github workflow

---
+++ build/builder.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+docker build -t temp:latest . -f "${PWD}"/build/Dockerfile
+
+docker build -t arpitjindal1997/apiserver:latest . -f "${PWD}"/build/apiserver/Dockerfile
+docker build -t arpitjindal1997/refresher:latest . -f "${PWD}"/build/refresher/Dockerfile
+
+docker push arpitjindal1997/apiserver:latest
+docker push arpitjindal1997/refresher:latest
