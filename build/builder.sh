978ca890baf116d84213cf3a35fb134390fb7efc
Arpit Agarwal <arpitjindal97@gmail.com>
2022-08-05 02:46:08 +0530

    Add EXPOSE in dockerfile
    Also add tracker

--- build/builder.sh
+++ build/builder.sh
@@ -5,5 +5,8 @@ docker build -t temp:latest . -f "${PWD}"/build/Dockerfile
 
 docker build -t arpitjindal1997/apiserver:latest . -f "${PWD}"/build/apiserver/Dockerfile
 docker build -t arpitjindal1997/refresher:latest . -f "${PWD}"/build/refresher/Dockerfile
+docker build -t arpitjindal1997/tracker:latest . -f "${PWD}"/build/tracker/Dockerfile
+
 docker push arpitjindal1997/apiserver:latest
 docker push arpitjindal1997/refresher:latest
+docker push arpitjindal1997/tracker:latest
