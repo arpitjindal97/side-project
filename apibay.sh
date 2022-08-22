1b8b15a3a910766067b01833fd6ded8300c32d57
Arpit Agarwal <arpitjindal97@gmail.com>
2022-08-22 19:58:24 +0530

    deploy dashboard to grafana

--- apibay.sh
+++ apibay.sh
@@ -1,13 +1,13 @@
 
-data=$(curl --silent 'https://apibay.org/precompiled/data_top100_200.json' | jq '.[].info_hash')
-data=$(curl --silent 'https://apibay.org/q.php?q=category%3A400' | jq '.[].info_hash')
+#data=$(curl --silent 'https://apibay.org/precompiled/data_top100_200.json' | jq '.[].info_hash')
+#data=$(curl --silent 'https://apibay.org/q.php?q=category%3A400' | jq '.[].info_hash')
 data=$(curl --silent 'https://apibay.org/q.php?q=category%3A200' | jq '.[].info_hash')
-data=$(curl --silent 'https://apibay.org/precompiled/data_top100_300.json' | jq '.[].info_hash')
+#data=$(curl --silent 'https://apibay.org/precompiled/data_top100_300.json' | jq '.[].info_hash')
 
 for info in $data
 do
 
-  curl --location --request POST 'http://188.166.76.22:30001/torrents' \
+  curl --location --request POST 'http://188.166.76.22:30001/api/torrents' \
   --header 'Content-Type: application/json' \
   --data-raw '{ "infohash":'$info' }'
   echo ""
