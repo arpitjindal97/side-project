dd583cbfbe1416c96e3b89b84ab59a7f6a4dd175
Arpit Agarwal <arpitjindal97@gmail.com>
2022-08-27 02:36:05 +0530

    minor change in bay script

--- apibay.sh
+++ apibay.sh
@@ -1,13 +1,14 @@
+#!/usr/bin/env bash
 
 #data=$(curl --silent 'https://apibay.org/precompiled/data_top100_200.json' | jq '.[].info_hash')
 #data=$(curl --silent 'https://apibay.org/q.php?q=category%3A400' | jq '.[].info_hash')
-data=$(curl --silent 'https://apibay.org/q.php?q=category%3A200' | jq '.[].info_hash')
-#data=$(curl --silent 'https://apibay.org/precompiled/data_top100_300.json' | jq '.[].info_hash')
+#data=$(curl --silent 'https://apibay.org/q.php?q=category%3A200' | jq '.[].info_hash')
+data=$(curl --silent 'https://apibay.org/precompiled/data_top100_300.json' | jq '.[].info_hash')
 
 for info in $data
 do
 
-  curl --location --request POST 'http://188.166.76.22:30001/api/torrents' \
+  curl --location --request POST 'http://vergon.tech:30001/api/torrents' \
   --header 'Content-Type: application/json' \
   --data-raw '{ "infohash":'$info' }'
   echo ""
